038e22b3051de92a331eb89d71653684d89ee65d
Pedro Morais <pedromorais2@hotmail.com>
2020-07-23 01:24:49 +0100

    simple controller

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+  }
+}
