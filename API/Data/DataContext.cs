e916773c4ce648ace1df9d424c270f7ab694cf89
Pedro Morais <pedromorais2@hotmail.com>
2020-07-23 01:25:18 +0100

    data dbcontext

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using API.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<Value> Values { get; set; }
+  }
+}
