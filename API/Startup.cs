a8f47fbc4f53eec46fc45c648508edfabd15960a
Pedro Morais <pedromorais2@hotmail.com>
2020-07-24 11:30:33 +0100

    CORS config

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,41 +13,37 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace API
-{
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
-    {
+namespace API {
+  public class Startup {
+    public Startup (IConfiguration configuration) {
       Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-      services.AddControllers();
+    public void ConfigureServices (IServiceCollection services) {
+      services.AddDbContext<DataContext> (options => options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
+      services.AddControllers ();
+      services.AddCors ();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
+    public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment ()) {
+        app.UseDeveloperExceptionPage ();
       }
 
-      app.UseHttpsRedirection();
+      app.UseHttpsRedirection ();
+      // Config Cors
+      app.UseCors (x => x.AllowAnyOrigin ().AllowAnyOrigin ().AllowAnyMethod ().AllowAnyHeader ());
 
-      app.UseRouting();
+      app.UseRouting ();
 
-      app.UseAuthorization();
+      app.UseAuthorization ();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
+      app.UseEndpoints (endpoints => {
+        endpoints.MapControllers ();
       });
     }
   }
